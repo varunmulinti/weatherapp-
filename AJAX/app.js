8346f94d1d300593460eae1a635687f495bedbf6
varunmulinti <varun9.run@gmail.com>
2016-04-23 11:14:54 +0530

    forecast almost done

--- AJAX/app.js
+++ AJAX/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	console.log('Document is ready');
 	$('#result').hide();
-	$('#forecast').click(function(){
+	$('#current').click(function(){
 		// Clear all old data
 		$('tbody>tr').remove();
 		var cityName=$('#input1').val();
 		// console.log(cityName);
-		var urlStr='http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=44db6a862fba0b067b1930da0d769e98';
+		var urlStr='http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid=27d43832d2a4adcb97fcbfa23db130aa';
 		console.log('Before calling Ajax');
 		$.ajax({
 		   url: urlStr,
@@ -18,7 +18,7 @@ $(document).ready(function(){
 		   	console.log(data);
 		   	var date=new Date(data.dt*1000);
 		   	var dateStr=date.getDate()+'-'+(date.getMonth()+1)+'-'+date.getFullYear();
-		   	var temparature=Math.round(data.main.temp-273.5,2);
+				var temparature=Math.round(data.main.temp-273.5,2);
 		   	var humidity=data.main.humidity;
 		   	var weather=data.weather[0].description;
 		   	$('#textInfo').text('Current Weather for city '+cityName+','+data.sys.country);
@@ -28,7 +28,27 @@ $(document).ready(function(){
 		   },
 		});
 	});
-	console.log('After registering click event');
-});
-
-console.log('Hello');
+	$('#forecast').click(function () {
+    $('tbody>tr').remove();
+    var cityName = $('#input1').val();
+    var forecastStr = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=bc1b6968884b3366859f9679983dc947';
+    $.ajax({
+      url: forecastStr,
+      error: function () {
+				console.log("error occured");
+			},
+      success: function(data) {
+          var date = new Date(data.dt * 1000);
+          var dateStr = date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear();
+          var timeStr = date.getHours() + ':' + date.getMinutes();
+          var temparature = Math.round(data.main.temp-273.5, 2);
+          var humidity = data.main.humidity;
+          var weather = data.weather[0].description;
+					$('#textInfo').text('Forecast for 5 days for city ' + cityName + ',' + data.city.country);
+          var innerHtml = '<tr><td>' + dateStr + '</td><td>' + timeStr + '</td><td>' + temparature + '</td><td>' + humidity + '</td><td>' + weather + '</td></tr>';
+          $('tbody').append(innerHtml);
+          $('#result').show();
+			}
+		});
+	});
+})
